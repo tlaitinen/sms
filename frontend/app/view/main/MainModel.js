37bd055b68e304a6088e130057be0b538684180a
Tero Laitinen <tero.laitinen@iki.fi>
2015-06-02 14:33:37 +0300

    ExtJS starter application

---
+++ frontend/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Receipts.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Receipts'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
---
+++ frontend/build/temp/development/Tiliduunari/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/tero/receipts/frontend/app.js
