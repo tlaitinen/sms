67750796444d558ba1ffb03bc6a42dfc496d7aa3
Tero Laitinen <tero.laitinen@iki.fi>
2015-06-08 10:14:49 +0300

    release script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DATE=`date +%Y-%m-%d`
+BASE=receipts-bin
+cd frontend
+sencha app build production
+cd ..
+pushd ~/$BASE
+git branch $DATE
+git checkout $DATE
+popd
+mkdir -p ~/$BASE/backend ~/$BASE/frontend
+cp backend/dist/build/backend/backend ~/$BASE/backend
+cp backend/dist/build/minuterun/minuterun ~/$BASE/backend
+cp -r frontend/build/production/Receipts/* ~/$BASE/frontend
+pushd ~/$BASE
+git add *
+git commit -m "$BASE version $DATE"
+git push origin $DATE
+git checkout master
+popd
