bd4b13f721cabf41f7dc2242d949377cdee9375a
Tero Laitinen <tero.laitinen@iki.fi>
2015-09-08 13:42:17 +0000

    install scripts to release directory

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d`
 BASE=sms-bin
+
 cd frontend
 sencha app build production
 cd ..
@@ -8,8 +9,10 @@ pushd ~/$BASE
 git branch $DATE
 git checkout $DATE
 popd
-mkdir -p ~/$BASE/backend ~/$BASE/frontend
+
+mkdir -p ~/$BASE/backend/scripts ~/$BASE/frontend
 cp ~/.local/bin/backend ~/$BASE/backend
+cp -r backend/scripts/* ~/$BASE/backend/scripts
 cp ~/.local/bin/minuterun ~/$BASE/backend
 cp -r frontend/build/production/SMS/* ~/$BASE/frontend
 pushd ~/$BASE
